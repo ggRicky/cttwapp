75d8becdcfa5331c2868435d345d533ffda69589
ggRicky <ricardogg67@gmail.com>
2017-07-28 00:05:29 -0500

    2017-07-28 [00:05 Hrs.] - Inicio del proyecto CTTwapp

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -9,7 +9,7 @@ $this->title = 'My Yii Application';
     <div class="jumbotron">
         <h1>CTT Exp. & Rentals - CTTWApp 1.0</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <p class="lead">Sistema de Gestion de Procesos de CTT.</p>
 
         <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
     </div>

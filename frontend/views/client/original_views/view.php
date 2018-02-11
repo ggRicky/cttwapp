ceac1c7ce00d1c333dfd05d1ad6330b67269cda5
ggRicky <ricardogg67@gmail.com>
2018-02-11 14:26:42 -0600

    2018-02-11 CTTwapp project [ Client / Client-Type ] advances v2

---
+++ frontend/views/client/original_views/view.php
@@ -0,0 +1,46 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Client */
+
+$this->title = $model->id;
+$this->params['breadcrumbs'][] = ['label' => 'Clients', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="client-view">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => 'Are you sure you want to delete this item?',
+                'method' => 'post',
+            ],
+        ]) ?>
+    </p>
+
+    <?= DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            'id',
+            'rfc',
+            'curp',
+            'moral:boolean',
+            'first_name',
+            'paternal_name',
+            'maternal_name',
+            'created_at',
+            'updated_at',
+            'created_by',
+            'updated_by',
+            'client_type_id',
+        ],
+    ]) ?>
+
+</div>

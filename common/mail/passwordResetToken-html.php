38176a3438ba76128139e89d3d5168554fc0ed42
ggRicky <ricardogg67@gmail.com>
2018-07-02 13:43:45 -0500

    2018-07-02 CTTwapp project [ Recovery Password System : Test 2 ]

--- common/mail/passwordResetToken-html.php
+++ common/mail/passwordResetToken-html.php
@@ -5,14 +5,8 @@ use yii\helpers\Html;
 /* @var $user common\models\User */
 
 $resetLink = Yii::$app->urlManager->createAbsoluteUrl(['site/reset-password', 'token' => $user->password_reset_token]);
-
-$asset = \frontend\assets\AppAsset::register($this);
-$baseUrl = $asset->baseUrl;
 ?>
 <div class="password-reset">
-
-    <p><img src="<?=$baseUrl?>/img/ctt-logo_1.png" class="ctt-logo"></p>
-
     <p><?= Yii::t('app','Hola').' : ' ?> <?= Html::encode($user->username) ?>,</p>
 
     <p><?= Yii::t('app','Por favor use el siguiente enlace para restablecer su contraseña').' : ' ?></p>

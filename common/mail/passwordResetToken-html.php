5fa5a5217b4419aae75824fb3ef49b47f4eb875c
ggRicky <ricardogg67@gmail.com>
2018-07-02 23:18:27 -0500

    2018-07-02 CTTwapp project [ Recovery Password System : Test 7 ]

--- common/mail/passwordResetToken-html.php
+++ common/mail/passwordResetToken-html.php
@@ -9,15 +9,15 @@ $resetLink = Yii::$app->urlManager->createAbsoluteUrl(['site/reset-password', 't
 ?>
 <div class="password-reset">
 
+    <!-- 2018-07-01 : Customizes the mail content in the reset-password message.-->
     <p><?= Yii::t('app','Hola').' : ' ?> <?= Html::encode($user->username) ?>,</p>
     <p><?= Yii::t('app','Por favor use el siguiente enlace para restablecer su contraseña').' : ' ?></p>
     <p><?= Html::a(Html::encode($resetLink), $resetLink) ?></p>
 
+    <!-- 2018-07-02 : Customizes the mail footer in the reset-password message.-->
     <br/>
-
     <div style="position: relative;"><img src="https://ctt-app.com/ctt-logo.png"></div>
-    <h3 style="font-family: Source Sans Pro, Helvetica Neue, Helvetica, Arial, sans-serif"><b>CTTwapp V1.0</b></h3>
-    <h4 style="font-family: Source Sans Pro, Helvetica Neue, Helvetica, Arial, sans-serif">Servicio de Soporte al Cliente</h4>
-    <h5 style="font-family: Source Sans Pro, Helvetica Neue, Helvetica, Arial, sans-serif"><em>TSR Development Software</em></h5>
+    <h3><b>CTTwapp V1.0</b></h3>
+    <h4>Servicio de Soporte al Cliente<br/><em>TSR Development Software</em></h4>
 
 </div>

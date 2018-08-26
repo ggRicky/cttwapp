92168d4022a501f975d9b20e1dfef062c10ac09b
ggRicky <ricardogg67@gmail.com>
2018-08-25 22:31:56 -0500

    2018-08-25 CTTwapp project [ Cookies tests v1 ]

--- frontend/controllers/ArticleController.php
+++ frontend/controllers/ArticleController.php
@@ -441,8 +441,16 @@ class ArticleController extends Controller
     public function actionSetPageSize($pageSize)
     {
         if (isset($pageSize)){  // If the parameter for page size has been set, then ....
-            $cookie = new \yii\web\Cookie(['name' => 'article-pageSize', 'value' => $pageSize]);   // Creates a new cookie and stores the page size value in it.
-            Yii::$app->getResponse()->getCookies()->add($cookie);
+            //$cookie = new \yii\web\Cookie(['name' => 'article-pageSize', 'value' => $pageSize]);   // Creates a new cookie and stores the page size value in it.
+            //Yii::$app->getResponse()->getCookies()->add($cookie);
+
+            $cookies = Yii::$app->response->cookies;
+            $cookies->add(new \yii\web\Cookie([
+                'name' => 'article-pageSize',
+                'value' => $pageSize,
+            ]));
+
+
         }
 
         // Apply and show the newly generated changes.

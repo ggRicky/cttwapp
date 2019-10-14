aa60ea2144cd3ab5f5d5779b09cc41752a95cc77
ggRicky <ricardogg67@gmail.com>
2019-10-13 21:57:00 -0500

    2019-10-13 CTTwapp project [ GitHub - Deploy Test : 14 ]

--- frontend/web/cttwapp_deploy.php
+++ frontend/web/cttwapp_deploy.php
@@ -13,7 +13,7 @@
     $commands = [
         'pwd',
         'whoami',
-        'git pull origin master 2>&1;' ,
+        'git pull origin master >   /tmp/git.log 2>&1;' ,
         // 2018-05-21 : Important. For a success execution to the next two commands, in the sudoers file add the next line :
         //              www-data ALL=(ALL) NOPASSWD: ALL
     ];

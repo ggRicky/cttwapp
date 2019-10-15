04fb64cdde3e7a087d04b4b624857382c9c69e6c
ggRicky <ricardogg67@gmail.com>
2019-10-15 12:17:32 -0500

    2019-10-15 CTTwapp project [ Checking the deploy script v1 ]

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -99,7 +99,7 @@ $randomBg = rand(1,11);
                         <th><?= Yii::t('app','Plataforma') ?></th><td><?= Yii::t('app','Yii versión 2.0') ?></td>
                     </tr>
                     <tr>
-                        <th><?= Yii::t('app','Última Actualización') ?></th><td>2019-10-15 &nbsp;&nbsp;&nbsp; 11:50 Hrs.</td>
+                        <th><?= Yii::t('app','Última Actualización') ?></th><td>2019-10-15 &nbsp;&nbsp;&nbsp; 12:15 Hrs.</td>
                     </tr>
                     </tbody>
                 </table>
